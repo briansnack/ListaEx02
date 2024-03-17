e4d0ccdd3201ac13da4fb9b4d60281b03f6ca4e7
Brian <snackbrian10@gmail.com>
2024-03-17 10:40:55 -0300

    primeiro commit

---
+++ Programa.java
@@ -0,0 +1,20 @@
+public class Programa {
+    public static void main(String[] args) {
+        // Ex01.executar();
+        // Ex02.executar();
+        // Ex03.executar();
+        // Ex04.executar();
+        // Ex05.executar();
+        // Ex06.executar();
+        // Ex07.executar();
+        // Ex08.executar();
+        // Ex09.executar();
+        // Ex10.executar();
+        // Ex11.executar();
+        // Ex12.executar();
+        // Ex13.executar();
+        // Ex14.executar();
+        // Ex15.executar();
+    }
+}
+
